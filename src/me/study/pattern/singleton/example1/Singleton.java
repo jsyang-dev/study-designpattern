a232784071b6c22b277ab9bc83f588d0131eb605
Jeongsu Yang <mycat83@gmail.com>
2020-01-03 23:49:35 +0900

    기능: 디자인 패턴 코드 추가
    * Singleton 패턴 클래스 다이어그램 추가
    * Singleton 패턴 예제 코드 추가

---
+++ src/me/study/pattern/singleton/example1/Singleton.java
@@ -0,0 +1,11 @@
+package me.study.pattern.singleton.example1;
+
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+    private Singleton() {                                 
+        System.out.println("인스턴스를 생성했습니다.");
+    }
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
