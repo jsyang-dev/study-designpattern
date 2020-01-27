9d32fcb40a7590e85090dc5a4a5758a79ec59cbb
Jeongsu Yang <mycat83@gmail.com>
2020-01-27 21:31:57 +0900

    기능: 디자인 패턴 코드 추가
    * Abstract Factory 패턴 클래스 다이어그램 추가
    * Abstract Factory 패턴 예제 코드 추가

---
+++ src/me/study/pattern/abstractfactory/example/factory/Tray.java
@@ -0,0 +1,12 @@
+package me.study.pattern.abstractfactory.example.factory;
+import java.util.Vector;
+
+public abstract class Tray extends Item {
+    protected Vector tray = new Vector();
+    public Tray(String caption) {
+        super(caption);
+    }
+    public void add(Item item) {
+        tray.add(item);
+    }
+}
