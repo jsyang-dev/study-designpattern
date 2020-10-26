42297952a9fb53b5bfa8c5d84cf69c43ccdd6cc8
Jeongsu Yang <mycat83@gmail.com>
2020-10-26 21:57:18 +0900

    기능: Builder 패턴

---
+++ src/me/study/pattern/builder/example2/Main.java
@@ -0,0 +1,13 @@
+package me.study.pattern.builder.example2;
+
+public class Main {
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new NutritionFacts
+                .Builder(240, 8)
+                .calories(100)
+                .sodium(35)
+                .carbohydrate(27)
+                .build();
+        System.out.println("cocaCola = " + cocaCola);
+    }
+}
