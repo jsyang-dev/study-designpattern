44e77e9053d875e9c611276fea6fae5217e40eb3
Jeongsu Yang <mycat83@gmail.com>
2022-01-28 22:05:39 +0900

    feat: 퍼사드 패턴 예제

---
+++ src/main/java/me/study/designpattern/facade/Client.java
@@ -0,0 +1,20 @@
+package me.study.designpattern.facade;
+
+public class Client {
+
+    public static void main(String[] args) {
+        EmailSettings emailSettings = new EmailSettings();
+        emailSettings.setHost("127.0.0.1");
+
+        EmailSender emailSender = new EmailSender(emailSettings);
+
+        EmailMessage emailMessage = new EmailMessage();
+        emailMessage.setFrom("keesun");
+        emailMessage.setTo("whiteship");
+        emailMessage.setCc("mycat");
+        emailMessage.setSubject("오징어게임");
+        emailMessage.setText("밖은 더 지옥이더라고..");
+
+        emailSender.sendEmail(emailMessage);
+    }
+}
---
+++ src/main/java/me/study/designpattern/facade/EmailSettings.java
@@ -0,0 +1,14 @@
+package me.study.designpattern.facade;
+
+public class EmailSettings {
+
+    private String host;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
